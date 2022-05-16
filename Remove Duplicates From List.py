08ccf785344fe4e1a50042e12c84cb3af8a982aa
declanquinn00 <quinndeclan00@gmail.com>
2022-05-16 13:19:41 +0100

    Better Solution

--- Remove Duplicates From List.py
+++ Remove Duplicates From List.py
@@ -3,30 +3,18 @@ nums2 = None
 nums3 = [1,1,2]
 nums4 = [0,0,1,1,1,2,2,3,3,4]
 def removeDuplicates(nums):
-    k = 0  # count duplicates
-    if (nums != None):
-        l = len(nums)
-        i = 1
-        prev = nums[0]
-        while i < l:
-            if (nums[i] == prev and nums[i]!= None):
-                j = i
-                nums[i] = None
-                k = k + 1
-                while j < l - 1:
-                    nums[j] = nums[j + 1]
-                    j = j + 1
-                if j == l - 1 and nums[j] != None:
-                    nums[j] = None
-                    j = j + 1
-            else:
-                prev = nums[i]
-                i = i + 1
-        k = l - k  # length - duplicates = sorted array length
-    return k
+    if(nums!=None):
+        j = 1 # j can never be a duplicate
+        for i in range(1,len(nums)):    # for each number
+            if nums[i-1] != nums[i]:    # if not a duplicate
+                nums[j] = nums[i]       # first set of numbers
+                j += 1                  # increace non duplicate list
+        return j
+    else:
+        return None
 
 print(removeDuplicates(nums1))
-print(removeDuplicates(nums2))
+#print(removeDuplicates(nums2))
 print(removeDuplicates(nums3))
 print(removeDuplicates(nums4))
 print("Done")
